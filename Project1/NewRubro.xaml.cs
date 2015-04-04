4bc95360c6b4871f2d136ab3a2f8730ea1c8232c
carlos727 <carlos72793@hotmail.com>
2015-04-04 18:31:58 -0500

    Last Commit

--- Project1/NewRubro.xaml.cs
+++ Project1/NewRubro.xaml.cs
@@ -26,25 +26,40 @@ namespace Project1
                 {
                     String username = (from user in context.User select user.name).FirstOrDefault();
                     int current_cycle = (from cycle in context.Cycle select cycle.ID).Max();
-                    Rubro rubro = new Rubro();
-                    rubro.name = name.Text;
-                    if (option1.IsChecked == true)
-                        rubro.type = option1.Content.ToString();
-                    else
-                        rubro.type = option2.Content.ToString();
-                                       
-                    if (current.Text.Length != 0)
-                        rubro.current = Int32.Parse(current.Text);
-                    else
-                        rubro.current = 0;
+                    IQueryable<Rubro> list = from rubros in context.Rubro select rubros;
+                    List<Rubro> RubroItems = list.ToList();
+                    int i = 0;
+                    bool sw = false;
+                    while (i < RubroItems.Count && !sw)
+                    {
+                        Rubro r = RubroItems.ElementAt(i);
+                        if (r.name == name.Text) sw = true;
+                        i++;
+                    }
+                    if (!sw)
+                    {
+                        Rubro rubro = new Rubro();
+                        rubro.name = name.Text;
+                        if (option1.IsChecked == true)
+                            rubro.type = option1.Content.ToString();
+                        else
+                            rubro.type = option2.Content.ToString();
 
-                    rubro.expected = Int32.Parse(expected.Text);
-                    rubro.cycle = current_cycle;
-                    context.Rubro.InsertOnSubmit(rubro);
-                    context.SubmitChanges();
-                    MessageBox.Show(username + ", The Rubro Was Added Successfully");
-                }
-                NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.RelativeOrAbsolute));
+                        if (current.Text.Length != 0)
+                            rubro.current = Int32.Parse(current.Text);
+                        else
+                            rubro.current = 0;
+
+                        rubro.expected = Int32.Parse(expected.Text);
+                        rubro.cycle = current_cycle;
+                        context.Rubro.InsertOnSubmit(rubro);
+                        context.SubmitChanges();
+                        MessageBox.Show(username + ", The Rubro Was Added Successfully");
+                        NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.RelativeOrAbsolute));
+                    }
+                    else
+                        MessageBox.Show(username + ", The Rubro already exists.");
+                }                
             }
             else
             {
